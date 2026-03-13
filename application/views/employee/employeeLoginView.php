ddfd73a9363ba4630fa2e36fa22f377e7db8a7d2
Satyajit Manna <satyajitmanna4>
2026-03-13 13:28:44 +0530

    fixed css

--- application/views/employee/employeeLoginView.php
+++ application/views/employee/employeeLoginView.php
@@ -11,8 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?= base_url() ?>css/login.css">
-
+  <link href="<?= base_url('css/employee/employeeLoginView.css') ?>" rel="stylesheet">
 </head>
 
 <body>
